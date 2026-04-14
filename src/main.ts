1308ed842016e5bfdfe4c2a9b949f95517ef90e9
Mykola Demyanenko <kdemyanenko@example.com>
2026-04-14 20:51:27 +0300

    add class-validator, configure global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { TrimPipe } from './common/pipes/trim.pipe'; // Імпортуємо наш Pipe
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Реєструємо глобальні пайпи
+  app.useGlobalPipes(
+    new TrimPipe(), // ПЕРШИМ (обрізаємо пробіли)
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(3000);
+  console.log(`Application is running on: http://localhost:3000`);
 }
 bootstrap();
